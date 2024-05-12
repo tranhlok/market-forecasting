e3bebb6f3ed35773d94e99f936128880f02b0cd0
Loc Tran <tranhlok@gmail.com>
2024-05-12 01:35:43 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project to apply Deep Learning Models on Stock Market Data with Python and TensorFlow',
+    author='Loc Tran',
+    license='MIT',
+)
