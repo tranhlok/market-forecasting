1c81e3b7ed337afc4f4451d6e0b848263926b6d8
Loc Tran <tranhlok@gmail.com>
2024-05-12 15:00:15 -0400

    initial data loading

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,65 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import requests
+import json
+import pandas as pd
+import os 
+import tqdm
+
+with open("src/data/config.json", "r") as file:
+    config = json.load(file)
+    API_KEY = config['api']['iex_key']
+    BASE_URL = config['api']['base_url']
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+RAW_DATA_PATH = "data/raw"
+
+def save_raw_stock_data_to_csv(symbol, selected_time_range="1y"):
+    # Prepare request parameters
+    params = {"token": API_KEY}
+    # Make the API request
+    response = requests.get(f"{BASE_URL}stock/{symbol}/chart/{selected_time_range}", params=params)
+    data = response.json()
+
+    if "error" not in data:
+            stock_data = pd.DataFrame(data)
+            if not stock_data.empty:
+                stock_data["date"] = pd.to_datetime(stock_data["date"])
+                stock_data.set_index("date", inplace=True)
+                
+                first_date = stock_data.index.min().strftime('%Y_%m_%d')
+                last_date = stock_data.index.max().strftime('%Y_%m_%d')
+
+                # Save the raw data to CSV
+                csv_filename = f"{symbol}_{first_date}_to_{last_date}.csv"
+
+                # Save the raw data to CSV
+                # csv_filename = f"{symbol}_{selected_time_range}.csv"
+                csv_path = RAW_DATA_PATH + "/" + csv_filename
+                stock_data.to_csv(csv_path)
+                print(f"Raw Data of {symbol} from {first_date} to {last_date} saved to {csv_path}")
+            else:
+                print(f"No available data for symbol: {symbol} for {selected_time_range} time range")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+def load_sp500_symbols(csv_path):
+    try:
+        df = pd.read_csv(csv_path)
+        return df['Symbol'].tolist()  # Assuming 'Symbol' is the column name holding the stock symbols
+    except Exception as e:
+        print(f"Error reading the CSV file: {e}")
+        return []
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+def fetch_all_sp500_stock_data(symbols, time_range="1y"):
+    # for symbol in tqdm(symbols, desc="Processing S&P 500 Companies"):   
+    for symbol in symbols:    
+        print(f"Fetching data for {symbol}...")
+        try:
+            save_raw_stock_data_to_csv(symbol, time_range)
+        except Exception as e:
+            print(f"Failed to fetch data for {symbol}: {e}")
+    print("Fetching completed!")
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+# Load symbols from the CSV
+sp500_symbols = load_sp500_symbols("data/interim/sp500_list.csv")
+print(sp500_symbols)
+# Fetch stock data for all loaded symbols
+fetch_all_sp500_stock_data(sp500_symbols, "max")
